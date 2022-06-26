efbeb88669e918d2999069fa1485c8604ac73cfd
Yusuf Oğuzay <93535901+oguzaybilir@users.noreply.github.com>
2022-06-26 14:14:31 +0300

    data augmentation

--- saglik/data_augmentation.py
+++ saglik/data_augmentation.py
@@ -2,11 +2,10 @@ import cv2
 import numpy as np
 import imutils
 import matplotlib.pyplot as plt
-import os
-from glob import glob
 from scipy import signal
 
-img = cv2.imread("C:\\Users\\oguza\\Documents\GitHub\\basic_txt_codes\saglik\\33910776596.png")
+
+img = cv2.imread("C:\\Users\\oguza\\Documents\GitHub\\basic_txt_codes\saglik\\0003.png")
 
 bilateral = cv2.bilateralFilter(img, 11, 75, 75)
 cv2.imshow("bilateral", bilateral)
@@ -20,9 +19,17 @@ wiener = signal.wiener(img, 5, 5)
 cv2.imshow("wiener", wiener)
 cv2.waitKey(0)
 
-cv2.imwrite("C:\\Users\\oguza\\Documents\GitHub\\basic_txt_codes\saglik\\33910776596_bilateral.png", bilateral)
-cv2.imwrite("C:\\Users\\oguza\\Documents\GitHub\\basic_txt_codes\saglik\\33910776596_gauss.png", gauss)
-cv2.imwrite("C:\\Users\\oguza\\Documents\GitHub\\basic_txt_codes\saglik\\33910776596_wiener3.png", wiener)
+siralanmis = np.vstack((img, bilateral, gauss, wiener))
+siralanmis2 = np.hstack((img, bilateral, gauss, wiener))
+cv2.imshow("siralanmis", siralanmis)
+cv2.imshow("siralanmis2", siralanmis2)
+cv2.waitKey(0)
+
+cv2.imwrite("C:\\Users\\oguza\\Documents\GitHub\\basic_txt_codes\saglik\\0003_bilateral.png", bilateral)
+cv2.imwrite("C:\\Users\\oguza\\Documents\GitHub\\basic_txt_codes\saglik\\0003_gauss.png", gauss)
+cv2.imwrite("C:\\Users\\oguza\\Documents\GitHub\\basic_txt_codes\saglik\\0003_wiener3.png", wiener)
+cv2.imwrite("C:\\Users\\oguza\\Documents\GitHub\\basic_txt_codes\saglik\\0003_siralanmis.png", siralanmis)
+cv2.imwrite("C:\\Users\\oguza\\Documents\GitHub\\basic_txt_codes\saglik\\0003_siralanmis2.png", siralanmis2)
 
 
 cv2.destroyAllWindows()
---
+++ saglik/dcmtopng.py
@@ -0,0 +1,58 @@
+import cv2
+import numpy as np
+import pydicom as PDCM
+
+def dicomdan_png(dicom_dosya_yolu):
+    dicom_resim= PDCM.read_file(dicom_dosya_yolu)
+
+    satirlar=dicom_resim.get(0x00280010).value
+    sutunlar=dicom_resim.get(0x00280011).value
+    
+    ornek_numarasi= int(dicom_resim.get(0x00200013).value)
+    
+    pencere_merezi=int(dicom_resim.get(0x00281050).value)
+    pencere_genişlik=int(dicom_resim.get(0x00281051).value)
+    
+    max=int((pencere_merezi+pencere_genişlik)/2)
+    min=int((pencere_merezi-pencere_genişlik)/2)
+
+    if (dicom_resim.get(0x00281052) is None):
+        kesisimi_yeniden_olceklendirme = 0
+
+    else:
+        kesisimi_yeniden_olceklendirme = int(dicom_resim.get(0x00281052).value)
+    
+    if (dicom_resim.get(0x00281053) is None):
+        eğimi_yeniden_olceklendirme = 1
+    
+    else:
+        eğimi_yeniden_olceklendirme = int(dicom_resim.get(0x00281053).value)
+
+    
+    yeni_gorsel=np.zeros((satirlar,sutunlar), np.uint8)
+    pikseller=dicom_resim.pixel_array
+
+    for i in range(satirlar):
+        for j in range(sutunlar):
+            piksel_degeri = pikseller[i,j]
+            yeni_piksel_degeri=piksel_degeri*eğimi_yeniden_olceklendirme + kesisimi_yeniden_olceklendirme
+
+            if (yeni_piksel_degeri > max):
+                yeni_gorsel[i][j]=255
+            elif (yeni_piksel_degeri < min):
+                yeni_gorsel[i][j]=0
+            else:
+                yeni_gorsel[i][j]=int(((yeni_piksel_degeri-min)/(max-min))*255)
+    
+    return yeni_gorsel, ornek_numarasi
+
+
+def main():
+    dicom_formatindaki_resim="C:\\Users\\HP\\Desktop\\dicom\\34442674993.dcm"
+    
+    png_formatindaki_hali , ornek_numarasi = dicomdan_png(dicom_formatindaki_resim)
+    cv2.imwrite(str(ornek_numarasi - 1).zfill(4) +".png",png_formatindaki_hali)
+    
+if __name__ == "_main_":
+    main()
+    print("Bitti")
